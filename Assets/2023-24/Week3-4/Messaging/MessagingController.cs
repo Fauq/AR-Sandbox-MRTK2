50e770a34cb2c2ef3511d5bbf0e62d4ec4e2bb4a
exsong <exsong@umich.edu>
2023-10-25 20:16:35 -0400

    idk man

--- Assets/2023-24/Week3-4/Messaging/MessagingController.cs
+++ Assets/2023-24/Week3-4/Messaging/MessagingController.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+/*
+Name: Emily Song
+Description: I have not a clue what this is and what this does >:(
+ */
 
 public class MessagingController : MonoBehaviour
 {
@@ -8,6 +14,14 @@ public class MessagingController : MonoBehaviour
     private Subscription<MessagesDeletedEvent> messagesDeletedEvent;
     private Subscription<MessagesEditedEvent> messagesEditedEvent;
     private Subscription<MessagesAddedEvent> messagesAddedEvent;
+
+    //ADDED STUFF EMILY
+    public GameObject messageButtonPrefab; // Reference to your button prefab
+    public Transform buttonListParent;
+    public List<GameObject> messageList = new List<GameObject>(); // List to store buttons
+    public List<int> idList = new List<int>(); //List to store Message indices
+    //private ScrollHandler scrollHandler = GameObject.Find("ScrollObjects").GetComponent<ScrollHandler>();
+    private ScrollHandler scrollHandler;
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +54,11 @@ public class MessagingController : MonoBehaviour
         //Debug.Log("Deleted");
         List<Message> deletedMessages = e.DeletedMessages; // Which messages were deleted (Look at their id's)
         // Update the UI to reflect the deleted messages
+        foreach (Message message in deletedMessages)
+        {
+            //could do binary search here
+            index = idList.BinarySearch(message.id);
+        }
     }
 
     private void OnMessagesEdited(MessagesEditedEvent e)
@@ -47,6 +66,11 @@ public class MessagingController : MonoBehaviour
         //Debug.Log("Edited");
         List<Message> editedMessages = e.EditedMessages; // Which messages were edited (Look at their id's)
         // Update the UI to reflect the edited messages
+        //foreach (Message message in editedMessages)
+        //{
+        //    GameObject button = messageList.Find(x => x.Find("Button"). == message.id);
+        //}
+
     }
 
     private void OnMessagesAdded(MessagesAddedEvent e)
@@ -54,5 +78,12 @@ public class MessagingController : MonoBehaviour
         //Debug.Log("Added");
         List<Message> newAddedMessages = e.NewAddedMessages; // Which messages are new
         // Update the UI to reflect the new messages
+        foreach (Message message in newAddedMessages)
+        {
+
+            //GameObject newButton = messageButtonPrefab;
+            //newButton.
+            //scrollHandler.HandleAddingButton(newButton)
+        }
     }
 }
